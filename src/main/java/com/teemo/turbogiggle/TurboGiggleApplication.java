bbb00b2ce0a7fbead1007232a8dd643ebbbe673c
wang jianxiang <773745033@qq.com>
2021-05-31 21:22:03 +0800

    初始化 deploy 项目

---
+++ src/main/java/com/teemo/turbogiggle/TurboGiggleApplication.java
@@ -0,0 +1,13 @@
+package com.teemo.turbogiggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboGiggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurboGiggleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teemo/turbogiggle/TurboGiggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teemo.turbogiggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboGiggleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
